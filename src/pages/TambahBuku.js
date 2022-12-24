6b080fde14ffb2627b957f300351ed7a5a10c5b8
fadilaaka <akafadila09@gmail.com>
2022-12-24 10:04:06 +0700

    Add loading tambah buku

--- src/pages/TambahBuku.js
+++ src/pages/TambahBuku.js
@@ -14,6 +14,7 @@ const TambahBuku = () => {
   const [idKategori, setIdKategori] = useState("");
   const [file, setFile] = useState();
   const [status, setStatus] = useState();
+  const [loading, setLoading] = useState(false);
 
   // const url = "http://localhost:5000";
   const url = "https://incredible-complete-soybean.glitch.me";
@@ -34,6 +35,7 @@ const TambahBuku = () => {
   };
 
   const submitData = (e) => {
+    setLoading(true);
     e.preventDefault();
     const formData = new FormData();
     formData.append("image", file);
@@ -51,6 +53,7 @@ const TambahBuku = () => {
       .then((res) => {
         console.log(res);
         setStatus(res.status);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
@@ -203,12 +206,32 @@ const TambahBuku = () => {
                 })}
             </select>
           </div>
-          <button
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Submit
-          </button>
+          {loading ? (
+            <button
+              type="button"
+              disabled
+              className={`inline-flex w-auto cursor-pointer select-none appearance-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:pointer-events-none disabled:opacity-75`}
+            >
+              <svg className="h-4 w-4 animate-spin" viewBox="3 3 18 18">
+                <path
+                  className="fill-blue-800"
+                  d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"
+                ></path>
+                <path
+                  className="fill-blue-100"
+                  d="M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z"
+                ></path>
+              </svg>
+              <span className="ml-2"> Loading...</span>
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          )}
         </form>
         {status === 201 ? (
           <div
